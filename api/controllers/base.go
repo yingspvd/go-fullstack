7f192843ad4f787db19d537b75684495841ddf68
yingspvd <supavadee.ying@mail.kmutt.ac.th>
2021-06-27 23:40:24 +0700

    New : base controller

---
+++ api/controllers/base.go
@@ -0,0 +1,42 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/yingspvd/go-fullstack/api/models"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
+	var err error
+
+	if Dbdriver == "mysql" {
+		DBURL := fmt.Sprint("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
+		server.DB, error = gorm.Open(Dbdriver, DBURL)
+		if err != nil {
+			fmt.Printf("Cannot connect to %s database", Dbdriver)
+			log.Fatal("This is the error : ", err)
+		} else {
+			fmt.Printf("We are connected to the %s database", Dbdriver)
+		}
+	}
+
+	server.DB.Debug().AutoMigrate(&models.User{}, &models.Post{})
+
+	server.Router = mux.NewRouter()
+
+	server.initializeRoutes()
+}
+
+func (server *Server) Run(addr string) {
+	fmt.Println("Listening to port 8080")
+	log.Fatal(http.ListenAndServe(addr, server.Router))
+}
